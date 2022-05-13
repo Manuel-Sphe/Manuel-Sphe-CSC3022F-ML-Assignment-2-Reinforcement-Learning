f071a4838c9584eb0d8c1f61b9ac47b33cf5cca6
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2022-05-13 23:26:43 +0200

    initialised the R array for rewards

--- src/Scenario1.py
+++ src/Scenario1.py
@@ -1,11 +1,12 @@
+from operator import ne
 from FourRooms import FourRooms
 import numpy as np
 
 
-# 12  x 12 is the size of the enviroment and we have 4 possible actions per state 
+# 11  x 11 is the size of the enviroment and we have 4 possible actions per state 
 
-Q_table = np.zeros((144,4),dtype=float)
-R = np.zeros((144,4),dtype=int)
+Q_table = np.zeros([121,4],dtype=float)
+R = np.zeros([121,4],dtype=int)
 
 
 directions = np.array([[0,-1],[0,1],[-1,0],[1,0]]) # UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3 
@@ -27,25 +28,50 @@ def Possible_Move(state: int) -> [int] :
 # for moving from one state to a new state given (state,action)
 def Move(state:int ,action: int) -> int:
     
-    x,y = state%12, state//12 # get the coordinates given a state 
+    x,y = state%11, state//11 # get the coordinates given a state 
     
     new_state = [x,y] + directions[action]
     
     new_state = np.array(new_state) # make this a numpy array
     
-    return new_state[1]*12 + new_state[0] # the new state s'
+    return new_state[1]*11 + new_state[0] # the new state s'
     
     
-def Reward() -> np.array([[int]]) :
+def Populate_R() -> np.array([[int]]) :
+    
+    # for populating the R table 
+    
+    for state in range(121):
+        x,y = state%11 , state//11
+        
+        # Punishing the agent when it's taking an invalid move
+        # This if for the states near the outer boarders 
+        
+        actSeq =[
+            
+            [x,y-1 if (y-1>=0 and  y-1< 11) else -1], # UP 
+            [x, y+1 if( y+1>=0 and y+1 < 11) else -1], # DOWN
+            [x-1 if (x-1>=0 and x-1<11) else -1,y], # LEFT 
+            [x+1 if(x+1>=0 and x+1<11) else -1 ,y], # RIGHT
+        ]
+                
+        for i,val in enumerate(actSeq):
+            if val[0]<0 or val[1]<0:
+                R[state,i] = -1
+    return R
     
     
+def inner_walls_R(old_state:int,new_state:int, action:int,grid_cell:int)->int:
     
-    
+    if old_state == new_state:
+        
     pass
 def main():
 
     # Create FourRooms Object
     fourRoomsObj = FourRooms('simple')
+    
+    #print(Populate_R())
 
     # This will try to draw a zero
 
