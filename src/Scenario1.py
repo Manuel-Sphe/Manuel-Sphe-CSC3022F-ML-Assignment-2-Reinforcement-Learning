7c2fe43f256493b35c3e626518132628bd393742
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2022-05-14 19:54:46 +0200

    Q-table initialised succesfully

--- src/Scenario1.py
+++ src/Scenario1.py
@@ -1,17 +1,27 @@
-from multiprocessing.spawn import old_main_modules
-from operator import ne
+import random
 from FourRooms import FourRooms
 import numpy as np
 
+ # 12  x 12 is the size of the enviroment and we have 4 possible actions per state 
+global R, directions
+Q_table = np.zeros((144,4),dtype=float)
+R = np.zeros((144,4),dtype=int)
+epochs = 100
+gamma = 0.8 
+epsilon = 1
+fourRoomsObj = FourRooms('simple')
+visits = np.zeros(144)
 
-# 11  x 11 is the size of the enviroment and we have 4 possible actions per state 
-
-Q_table = np.zeros([121,4],dtype=float)
-R = np.zeros([121,4],dtype=int)
-
+decay = 1
+epsilon_decay = epochs // 2
 
 directions = np.array([[0,-1],[0,1],[-1,0],[1,0]]) # UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3 
 
+
+
+def Eploration():
+    
+    pass
 # for getting a listf of possible moves given a state 
 def Possible_Move(state: int) -> [int] :
     """
@@ -29,13 +39,13 @@ def Possible_Move(state: int) -> [int] :
 # for moving from one state to a new state given (state,action)
 def Move(state:int ,action: int) -> int:
     
-    x,y = state%11, state//11 # get the coordinates given a state 
+    x,y = state%12, state//12 # get the coordinates given a state 
     
     new_state = [x,y] + directions[action]
     
     new_state = np.array(new_state) # make this a numpy array
     
-    return new_state[1]*11 + new_state[0] # the new state s'
+    return new_state[1]*12 + new_state[0] # the new state s'
     
     
 def Populate_R() -> np.array([[int]]) :
@@ -43,17 +53,17 @@ def Populate_R() -> np.array([[int]]) :
     # for populating the R table 
     
     for state in range(121):
-        x,y = state%11 , state//11
+        x,y = state%12 , state//12
         
         # Punishing the agent when it's taking an invalid move
         # This if for the states near the outer boarders 
         
         actSeq =[
             
-            [x,y-1 if (y-1>=0 and  y-1< 11) else -1], # UP 
-            [x, y+1 if( y+1>=0 and y+1 < 11) else -1], # DOWN
-            [x-1 if (x-1>=0 and x-1<11) else -1,y], # LEFT 
-            [x+1 if(x+1>=0 and x+1<11) else -1 ,y], # RIGHT
+            [x,y-1 if (y-1>=0 and  y-1< 12) else -1], # UP 
+            [x, y+1 if( y+1>=0 and y+1 < 12) else -1], # DOWN
+            [x-1 if (x-1>=0 and x-1<12) else -1,y], # LEFT 
+            [x+1 if(x+1>=0 and x+1<12) else -1 ,y], # RIGHT
         ]
                 
         for i,val in enumerate(actSeq):
@@ -63,56 +73,68 @@ def Populate_R() -> np.array([[int]]) :
     
     
 def inner_walls_R(old_state:int,new_state:int, action:int,grid_cell:int)->int:
-    res = None
     if old_state == new_state: # if hits the inner wall 
         R[new_state,action] = -1 # punish the agent
-        res = R[old_state,action]
+        return R[old_state,action]
     else:
         R[old_state,action] = grid_cell
-        res = R[old_state,action] + 1 # reward with 1
+        return R[old_state,action] + 1 # reward with 1
     
-    return res
+def Learning_Q(visits:np.array([[int]]),discout:int = 0.8) -> int:
+        x,y = fourRoomsObj.getPosition()
+        #print(x,y)
+        state = y*12 + x # convert to 1D state 
+       # print(f'state {state}')
+        done = False
         
+        while not done :
+            visits[state] += 1
+            alpha = 1/visits[state]
+            state_action = Possible_Move(state)
+            
+            if random.random() < epsilon:
+                action = state_action[np.random.randint(0, len(state_action))]
+            else:
+                action = Q_table[state,:].argmax(0)
+            #print('action',action)
+            #next_state = Move(state,action)
+            
+            gridCell, current_pos , gType, isTerminal = fourRoomsObj.takeAction(action)
+            
+            inner_walls_R(state,current_pos[1]*12 + current_pos[0],action,gridCell)
+            next_state  = current_pos[1]*12 + current_pos[0]
+            #print(f'state {state} and next_state {next_state}')
+            #Q_table[state,action] += alpha *(gridCell + discout*(np.max(Q_table[next_state]) - Q_table[state,action]))   
+            
+            old_value = Q_table[state, action]
+            next_max = np.max(Q_table[next_state])
+        
+            new_value = (1 - alpha) * old_value + alpha * (gridCell + gamma * next_max)
+            Q_table[state, action] = new_value
+
+            done = isTerminal         
+            state = current_pos[1]*12 + current_pos[0]
+            
+      
+            
+           
+
+                
 def main():
+    
+   
 
     # Create FourRooms Object
-    fourRoomsObj = FourRooms('simple')
     
-    #print(Populate_R())
-
-    # This will try to draw a zero
-
-    """
-    actSeq = [FourRooms.LEFT, FourRooms.LEFT, FourRooms.LEFT,
-            FourRooms.UP, FourRooms.UP, FourRooms.UP,
-            FourRooms.RIGHT, FourRooms.RIGHT, FourRooms.RIGHT,
-            FourRooms.DOWN, FourRooms.DOWN, FourRooms.DOWN]
-
-
-
-    aTypes = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-    gTypes = ['EMPTY', 'RED', 'GREEN', 'BLUE']
-
-    print('Agent starts at: {0}'.format(fourRoomsObj.getPosition()))
-
-    for act in actSeq:
-        gridType, newPos, packagesRemaining, isTerminal = fourRoomsObj.takeAction(act)
-
-        print("Agent took {0} action and moved to {1} of type {2}".format (aTypes[act], newPos, gTypes[gridType]))
-        
-       
-
-        if isTerminal:
-            break
-
-    # Don't forget to call newEpoch when you start a new simulation run
-
-
-    # Show Path
-    fourRoomsObj.showPath(-1)
-    """
-  
-
+    R = Populate_R()
+    
+    for epoch in range(1,epochs):
+        Learning_Q(visits)
+        fourRoomsObj.newEpoch()
+    print(Q_table)
+    #print(Q_table[143])
+    
+    # Explole 
 
 if __name__ == "__main__":
     main()
