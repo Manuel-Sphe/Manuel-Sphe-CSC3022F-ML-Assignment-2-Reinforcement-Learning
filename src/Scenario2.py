158a7a5bcd34c55b86a61c57dae64571cefa0355
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2022-05-15 03:42:24 +0200

    Hyperparameters added

---
+++ src/Scenario2.py
@@ -0,0 +1,30 @@
+from FourRooms import FourRooms
+import random
+import numpy as np
+
+ # 12  x 12 is the size of the enviroment and we have 4 possible actions per state 
+global R, directions
+Q_table = np.zeros((3,144,4),dtype=float)
+R = np.zeros((3,144,4),dtype=int)
+epochs = 100
+gamma = 0.8 
+epsilon = 1
+fourRoomsObj = FourRooms('simple')
+visits = np.zeros(3,144) # 
+
+decay = 1
+epsilon_decay = epochs // 2
+
+directions = np.array([[0,-1],[0,1],[-1,0],[1,0]]) # UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3 
+
+
+def main():
+
+    # Create FourRooms Object
+    fourRoomsObj = FourRooms('multi')
+    print(R.shape)
+   
+
+
+if __name__ == "__main__":
+    main()
